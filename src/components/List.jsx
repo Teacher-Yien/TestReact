258a5c375c106709e8b8f7c61635c4980f1e804a
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-08-20 22:17:41 +0700

    add

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,6 +3,7 @@ function List(){
 	return(
 		<div className=" container">
 		   <h1>Hello Sis Cute</h1>
+			 <h2>Hello momo</h2>
 			 <img className=" rounded"  src="assets/login.png" alt="" />
 		</div>
 	)
