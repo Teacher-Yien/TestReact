c27205a9be406247a7d743d2ea936e2e52e11ae8
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-08-20 22:04:43 +0700

    add lg

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+
+function List(){
+	return(
+		<div className=" container">
+		   <h1>Hello Sis Cute</h1>
+			 <img className=" rounded"  src="assets/login.png" alt="" />
+		</div>
+	)
+}
+export default List
